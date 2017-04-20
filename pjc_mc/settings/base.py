ea2783d62733dd2103841a9c5188aea74e86f2bd
Eric Pascual <eric@pobot.org>
2017-04-21 01:28:12 +0200

    whitenoise settings fixed

--- pjc_mc/settings/base.py
+++ pjc_mc/settings/base.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third-party
-    'whitenoise',
     'bootstrap3',
     # application
     'teams.config.TeamsAppConfig',
@@ -55,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
