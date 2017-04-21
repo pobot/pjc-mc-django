2027d46392bfa28d3dc28b4ff49040d412e8757e
Eric Pascual <eric@pobot.org>
2017-04-21 15:30:15 +0200

    added Fabric

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,31 @@
 # -*- coding: utf-8 -*-
 
+from fabric.api import task, env
+from fabric.contrib.project import rsync_project
+
 __author__ = 'Eric Pascual'
+
+env.hosts = ['eric-laptop.local']
+env.use_ssh_config = True
+
+
+@task
+def deploy():
+    rsync_project(
+        remote_dir='pjc-mc/',
+        local_dir='./',
+        exclude=[
+            '.*',
+            'fabfile.py',
+            '*.pyc',
+            '__pycache__',
+            'db.sqlite3',
+            'data',
+            'requirements-dev.txt',
+            'staticfiles',
+            'teams.txt',
+            'tests',
+            'pytest.ini',
+        ],
+        default_opts='-arh'
+    )
