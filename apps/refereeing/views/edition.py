d4a080f9456d884a3ca36e213e273b21b1845fcb
Eric Pascual <eric@pobot.org>
2017-11-06 01:04:14 +0100

    update for 2018 edition

--- apps/refereeing/views/edition.py
+++ apps/refereeing/views/edition.py
@@ -9,27 +9,16 @@ __author__ = 'Eric Pascual'
 class Robotics1CreateView(RoboticsBaseView):
     match_num = 1
     form_class = robotics_form_class(match_num)
+    multi_trials_allowed = True
 
 
 class Robotics2CreateView(RoboticsBaseView):
     match_num = 2
     form_class = robotics_form_class(match_num)
-    used_time_field = 'used_time'
+    multi_trials_allowed = True
 
 
 class Robotics3CreateView(RoboticsBaseView):
     match_num = 3
     form_class = robotics_form_class(match_num)
-    random_configuration = True
-
-    @classmethod
-    def get_random_config(cls):
-        last_choice = set()
-        cfg = []
-        for _ in range(3):
-            possible = list({1, 2, 3} - last_choice)
-            choice = random.choice(possible)
-            cfg.append(choice)
-            last_choice = {choice}
-
-        return cfg
+    multi_trials_allowed = True
