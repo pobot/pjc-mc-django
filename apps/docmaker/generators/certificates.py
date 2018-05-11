d3000478290c9c5e1b7f1a4819835a2f5d16500d
Eric Pascual <eric@pobot.org>
2018-05-11 12:41:22 +0200

    SVG templating revisited, tmp files cleanup added

--- apps/docmaker/generators/certificates.py
+++ apps/docmaker/generators/certificates.py
@@ -24,18 +24,37 @@ class CertificateGenerator(ReportGenerator):
         with open(os.path.join(ASSETS_DIR, self.svg_template_file)) as fp:
             templ = '\n'.join(fp.readlines())
 
+        # transform the SVG XML code by something usable as a string template
+        # - escape percents (used in dimensions for instance)
+        # - transform our pseudo vars into format placeholders
+        templ = templ.replace("%", "%%").replace("$(", "%(")
+
         competition_date = settings.PJC['event_date']
+        edition = settings.PJC['edition']
+
+        tmp_files = []
+
+        tmp_svg = "/tmp/certificate.svg"
+        tmp_files.append(tmp_svg)
 
         for team in Team.objects.all():
-            tmp_svg = "/tmp/certificate.svg"
             tmp_pdf_file_path = os.path.join("/tmp", "%s-%02d.pdf" % (self.output_file_name, team.num))
             with open(tmp_svg, 'w') as fp:
-                fp.write(templ.replace('%(team_name)s', team.name).replace("%(date)s", competition_date))
+                fp.write(templ % {
+                    'team_name': team.name,
+                    'date': competition_date,
+                    'edition': edition
+                })
             subprocess.call("inkscape %s -A %s > /dev/null 2>&1" % (tmp_svg, tmp_pdf_file_path), shell=True)
+            tmp_files.append(tmp_pdf_file_path)
 
         pdf_file_name = self.output_file_name + '.pdf'
         subprocess.call("pdftk /tmp/%s-??.pdf cat output %s" % (
             self.output_file_name, os.path.join(self._output_dir, pdf_file_name)
         ), shell=True)
+
+        # cleanup
+        for p in tmp_files:
+            os.remove(p)
 
         return pdf_file_name
