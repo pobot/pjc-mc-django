662be2daaa167024be498164019bddfae9536b16
Eric Pascual <eric@pobot.org>
2019-03-29 00:04:16 +0100

    adds style to status field

--- apps/volunteers/admin.py
+++ apps/volunteers/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.conf import settings
+from django.utils.safestring import mark_safe
 
 from .models import *
 
@@ -18,6 +19,24 @@ volunteer_compose_email_action.short_description = "Composer un email"
 
 @admin.register(Volunteer)
 class VolunteerAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'email', 'status', 'present']
+    class Media:
+        css = {
+            'all': ['css/volunteers/admin.css']
+        }
+
+    list_display = ['__str__', 'email', 'status_styled', 'present']
     actions = [volunteer_compose_email_action]
     list_filter = ['status', 'present']
+
+    status_styles = {
+        '?': 'unknown',
+        'O': 'avail',
+        'N': 'notavail'
+    }
+
+    def status_styled(self, obj: Volunteer):
+        value = obj.get_status_display()
+        color = self.status_styles[obj.status]
+        return mark_safe('<span class="%s">%s</span>' % (color, value))
+
+    status_styled.short_description = 'Réponse'
