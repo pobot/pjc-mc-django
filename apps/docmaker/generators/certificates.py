4431c749190039a11a4060aa59f1c3e724ec2701
Eric Pascual <eric@pobot.org>
2019-05-06 23:49:43 +0200

    replace broken pdftk by PyPDF2 lib for cat operation

--- apps/docmaker/generators/certificates.py
+++ apps/docmaker/generators/certificates.py
@@ -4,6 +4,9 @@ import os
 import subprocess
 
 from django.conf import settings
+from django.core.management import CommandError
+
+from PyPDF2 import PdfFileMerger
 
 from docmaker.commons import ReportGenerator, ASSETS_DIR
 
@@ -21,7 +24,11 @@ class CertificateGenerator(ReportGenerator):
     svg_template_file = "PJC_certificat_participation.svg"
 
     def generate(self):
-        with open(os.path.join(ASSETS_DIR, self.svg_template_file)) as fp:
+        svg_template_path = os.path.join(ASSETS_DIR, self.svg_template_file)
+        if not os.path.exists(svg_template_path):
+            raise CommandError('SVG template file not found at %s' % svg_template_path)
+
+        with open(svg_template_path) as fp:
             templ = '\n'.join(fp.readlines())
 
         # transform the SVG XML code by something usable as a string template
@@ -32,12 +39,12 @@ class CertificateGenerator(ReportGenerator):
         competition_date = settings.PJC['event_date']
         edition = settings.PJC['edition']
 
-        tmp_files = []
+        team_certificates = []
 
         tmp_svg = "/tmp/certificate.svg"
-        tmp_files.append(tmp_svg)
+        to_be_cleaned = [tmp_svg]
 
-        for team in Team.objects.all():
+        for team in Team.objects.all().order_by('num'):
             tmp_pdf_file_path = os.path.join("/tmp", "%s-%02d.pdf" % (self.output_file_name, team.num))
             with open(tmp_svg, 'w') as fp:
                 fp.write(templ % {
@@ -46,15 +53,25 @@ class CertificateGenerator(ReportGenerator):
                     'edition': edition
                 })
             subprocess.call("inkscape %s -A %s > /dev/null 2>&1" % (tmp_svg, tmp_pdf_file_path), shell=True)
-            tmp_files.append(tmp_pdf_file_path)
+            to_be_cleaned.append(tmp_pdf_file_path)
+            team_certificates.append(tmp_pdf_file_path)
 
         pdf_file_name = self.output_file_name + '.pdf'
-        subprocess.call("pdftk /tmp/%s-??.pdf cat output %s" % (
-            self.output_file_name, os.path.join(self._output_dir, pdf_file_name)
-        ), shell=True)
+        in_files = [open(fpath, 'rb') for fpath in team_certificates]
+        try:
+            merger = PdfFileMerger()
+            for fp in in_files:
+                merger.append(fp)
+
+            with open(os.path.join(self._output_dir, pdf_file_name), 'wb') as out_file:
+                merger.write(out_file)
+
+        finally:
+            for fp in in_files:
+                fp.close()
 
         # cleanup
-        for p in tmp_files:
+        for p in to_be_cleaned:
             os.remove(p)
 
         return pdf_file_name
