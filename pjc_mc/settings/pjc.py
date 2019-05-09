ffbca15b121f0d29aec100f46d6fa9c5dad4b45b
Eric Pascual <eric@pobot.org>
2019-05-09 15:37:09 +0200

    ending times updated

--- pjc_mc/settings/pjc.py
+++ pjc_mc/settings/pjc.py
@@ -11,10 +11,10 @@ PJC = {
     'end_time': "17:30",
     'planning_slot_minutes': 10,
     'time_limits': [
-        datetime.time(15, 00),  # time limit for round 1 matches
-        datetime.time(16, 00),  # time limit for round 2 matches
-        datetime.time(17, 00),  # time limit for round 3 matches
-        datetime.time(17, 00)  # time limit for presentations
+        datetime.time(14, 00),  # time limit for round 1 matches
+        datetime.time(14, 45),  # time limit for round 2 matches
+        datetime.time(15, 30),  # time limit for round 3 matches
+        datetime.time(16, 00)   # time limit for presentations
     ],
     'display_page_size': 10,
     'display_next_schedules_count': 9,
